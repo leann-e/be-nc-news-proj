01adfc8b305202419d6b1c0b300906b7fec21dd6
leann-e <leannewong15@gmail.com>
2022-08-02 10:21:20 +0100

    update tests to add seed, delete playground.sql

--- __tests__/ncnews.test.js
+++ __tests__/ncnews.test.js
@@ -1,6 +1,7 @@
 const request = require("supertest");
 const app = require("../app");
 const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
 
 describe("GET", () => {
   describe("GET/api/topics", () => {

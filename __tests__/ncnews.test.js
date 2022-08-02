5ee07c2f756c776c38f831bb6e684450ce9442cd
leann-e <leannewong15@gmail.com>
2022-08-02 10:34:28 +0100

    merged two tests to be one

--- __tests__/ncnews.test.js
+++ __tests__/ncnews.test.js
@@ -5,19 +5,12 @@ const seed = require("../db/seeds/seed");
 
 describe("GET", () => {
   describe("GET/api/topics", () => {
-    test("should respond with an object that contains an array", () => {
-      return request(app)
-        .get("/api/topics")
-        .expect(200)
-        .then(({ body }) => {
-          expect(Array.isArray(body.topics)).toBe(true);
-        });
-    });
     test("status: 200, responds with an array of topic objects, with a slug and description property", () => {
       return request(app)
         .get("/api/topics")
         .expect(200)
         .then(({ body }) => {
+          expect(Array.isArray(body.topics)).toBe(true);
           expect(body.topics).toHaveLength(3);
           body.topics.forEach((topic) => {
             expect(topic).toEqual(
